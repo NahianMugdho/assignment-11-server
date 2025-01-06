b4e60fd4cb03d8e5c2d7b0a13f4f8ccc759a6249
NahianMugdho <nahianmugdho@gmail.com>
2025-01-06 09:30:40 +0600

    'data-send'

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ require('dotenv').config()
 
 
 
-
-
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.spfvw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,6 +28,22 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+//products
+
+const productCollection = client.db('product').collection('server');
+
+app.get('/product',async(req,res)=>{
+  const cursor = productCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
+
+
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
