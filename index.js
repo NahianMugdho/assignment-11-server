3f5b21d638920d71d3ab14cdf5260719c41d1748
NahianMugdho <nahianmugdho@gmail.com>
2025-01-08 03:27:44 +0600

    'deployed'

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 
-require('dotenv').config()
+
 
 
 
